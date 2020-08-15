8e0f1e515339cedb2538ab09461536f09f53c2da
Meng-Chuan Chang <meng.chuan.chang@gmail.com>
2020-08-15 17:02:24 +0800

    add User Profile Page

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router 		= express.Router();
 var passport 	= require("passport");
 var User 		= require("../models/user");
 var middleware 	= require("../middleware");
+var Campground 	= require("../models/campground");
 
 router.get("/", function(req, res){
 	res.render("landing");
@@ -20,7 +21,13 @@ router.get("/register", function(req, res){
 // Handle sign up logic
 
 router.post("/register", function(req, res){
-	var newUser = new User({username: req.body.username});
+	var newUser = new User({
+		username: req.body.username,
+		firstName: req.body.firstName,
+		lastName: req.body.lastName,
+		email: req.body.email,
+		avatar: req.body.avatar
+	});
 	// eval(require("locus")) // Stop the code, so developer could take a look
 	if(req.body.adminCode === 'secretCode') {
 		newUser.isAdmin = true;
@@ -56,6 +63,25 @@ router.get("/logout", function(req, res){
 	req.logout();
 	req.flash("success", "Logged you out!")
 	res.redirect("/campgrounds");
+});
+
+// USER PROFILE
+router.get("/users/:id", function(req, res){
+	User.findById(req.params.id, function(err, foundUser){
+		if(err){
+			req.flash("error", "Something wet wrong.");
+			res.redirect("/");
+		} else {
+			Campground.find().where('author.id').equals(foundUser._id).exec(function(err, campgrounds){
+				if(err){
+					req.flash("error", "Something wet wrong.");
+					res.redirect("/");
+				} else {
+					res.render("users/show", {user: foundUser, campgrounds: campgrounds});
+				}
+			});
+		}
+	});
 });
 
 module.exports = router;
